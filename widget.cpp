843d5b06792f82de8daa49f9d9d6bebd5c8096c1
MarioUnoDueTreQuattro <andreagi0@yahoo.it>
2025-10-07 14:55:55 +0200

    Many changes: custom buttons, transparent, on top, resize...

--- widget.cpp
+++ widget.cpp
@@ -7,7 +7,7 @@
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
-    , ui(new Ui::Widget)
+    , ui(new Ui::Widget), mousePressed(false), resizing(false), resizeMargin(4), topButtonsMargin(3)
 {
     ui->setupUi(this);
     setWindowTitle ("Volume");
@@ -22,11 +22,14 @@ Widget::Widget(QWidget *parent)
     // );
     Qt::WindowFlags flags = windowFlags();
     // 2. Remove the specific hints for minimize and maximize
-    flags &= ~Qt::WindowMinimizeButtonHint;
+    //flags |= Qt::Tool;
+    //flags &= ~Qt::WindowMinimizeButtonHint;
     flags &= ~Qt::WindowMaximizeButtonHint;
     //flags &= ~ Qt::WindowCloseButtonHint;
     // 3. Set the modified flags back
     setWindowFlags(flags);
+    setWindowFlags(Qt::FramelessWindowHint);
+    //setWindowFlags(Qt::Tool | Qt::CustomizeWindowHint);
     m_systemVolumeController = new SystemVolumeController(this);
     connect(m_systemVolumeController, SIGNAL(volumeChanged(float)),
         this, SLOT(onSystemVolumeChanged(float)));
@@ -51,6 +54,30 @@ Widget::Widget(QWidget *parent)
     // ui->muteButton->setChecked (false);
     // }
     updateMuteButtonIcon ();
+    closeButton = new QPushButton("X", this);
+    closeButton->setGeometry(width() - 10, topButtonsMargin, 10, 10);
+    closeButton->setStyleSheet("QPushButton { background-color: red; color: white; border: none; }");
+    minimizeButton = new QPushButton("-", this);
+    minimizeButton->setGeometry(width() - 20, topButtonsMargin, 10, 10);
+    minimizeButton->setStyleSheet("QPushButton { background-color: gray; color: white; border: none; }");
+    onTopButton = new QPushButton("↑", this);
+    onTopButton->setGeometry(width() - 30, topButtonsMargin, 10, 10);
+    onTopButton->setStyleSheet("QPushButton { background-color: green; color: white; border: none; }");
+    transparentButton = new QPushButton("T", this);
+    transparentButton->setGeometry(width() - 40, topButtonsMargin, 10, 10);
+    transparentButton->setStyleSheet("QPushButton { background-color: blue; color: white; border: none; }");
+    connect(transparentButton, SIGNAL(clicked()), this, SLOT(toggleTransparency()));
+    connect(onTopButton, SIGNAL(clicked()), this, SLOT(toggleOnTop()));
+    connect(closeButton, SIGNAL(clicked()), this, SLOT(close()));
+    connect(minimizeButton, SIGNAL(clicked()), this, SLOT(showMinimized()));
+    closeButton->setToolTip ("Close");
+    minimizeButton->setToolTip ("Minimize");
+    onTopButton->setToolTip ("On top");
+    transparentButton->setToolTip ("Transparent");
+    closeButton->setFixedSize (10, 10);
+    minimizeButton->setFixedSize (10, 10);
+    onTopButton->setFixedSize (10, 10);
+    transparentButton->setFixedSize (10, 10);
     loadSettings ();
     // adjustSize();
 }
@@ -67,8 +94,126 @@ void Widget::closeEvent(QCloseEvent *event)
     QWidget::closeEvent(event);
 }
 
+void Widget::resizeEvent(QResizeEvent *event)
+{
+    // 1. Chiami la versione base del gestore di eventi
+    QWidget::resizeEvent (event);
+    closeButton->setGeometry(width() - 10, topButtonsMargin, 10, 10);
+    minimizeButton->setGeometry(width() - 20, topButtonsMargin, 10, 10);
+    onTopButton->setGeometry(width() - 30, topButtonsMargin, 10, 10);
+    transparentButton->setGeometry(width() - 40, topButtonsMargin, 10, 10);
+    // 2. Imposti il vincolo di larghezza
+    // this->setFixedWidth (48);
+    // 3. ✨ Forza il genitore ad aggiornare la sua dimensione preferita
+    // Questo costringe Qt a ricalcolare i layout, tenendo conto del setFixedWidth
+    // appena impostato.
+    // this->updateGeometry(); // O, in alternativa,
+    // this->adjustSize();
+}
+
+void Widget::mousePressEvent(QMouseEvent *event)
+{
+    // qDebug() << __PRETTY_FUNCTION__;
+    QRect rect = this->rect();
+    QPoint pos = event->pos();
+    if (pos.x() <= resizeMargin && pos.y() <= resizeMargin)
+        setCursor(Qt::SizeFDiagCursor); // Top-left
+    else if (pos.x() >= rect.width() - resizeMargin && pos.y() <= resizeMargin)
+        setCursor(Qt::SizeBDiagCursor); // Top-right
+    else if (pos.x() <= resizeMargin && pos.y() >= rect.height() - resizeMargin)
+        setCursor(Qt::SizeBDiagCursor); // Bottom-left
+    else if (pos.x() >= rect.width() - resizeMargin && pos.y() >= rect.height() - resizeMargin)
+        setCursor(Qt::SizeFDiagCursor); // Bottom-right
+    else if (pos.x() <= resizeMargin)
+        setCursor(Qt::SizeHorCursor); // Left
+    else if (pos.x() >= rect.width() - resizeMargin)
+        setCursor(Qt::SizeHorCursor); // Right
+    else if (pos.y() <= resizeMargin)
+        setCursor(Qt::SizeVerCursor); // Top
+    else if (pos.y() >= rect.height() - resizeMargin)
+        setCursor(Qt::SizeVerCursor); // Bottom
+    else
+        setCursor(Qt::ArrowCursor); // Default
+    // Store the position where the mouse was pressed
+    if (event->button() == Qt::LeftButton)
+    {
+        mousePressed = true;
+        mouseStartPos = event->globalPos();
+        windowStartRect = geometry();
+        // Detect if mouse is near border
+        resizing = false;
+        resizeDirection = Qt::Edges();
+        if (pos.x() <= resizeMargin)
+            resizeDirection |= Qt::LeftEdge;
+        else if (pos.x() >= rect.width() - resizeMargin)
+            resizeDirection |= Qt::RightEdge;
+        if (pos.y() <= resizeMargin)
+            resizeDirection |= Qt::TopEdge;
+        else if (pos.y() >= rect.height() - resizeMargin)
+            resizeDirection |= Qt::BottomEdge;
+        if (resizeDirection != Qt::Edges())
+            resizing = true;
+    }
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *event)
+{
+    // qDebug() << __PRETTY_FUNCTION__;
+    updateCursorShape(event->pos());
+    QWidget *child = childAt(mapFromGlobal(QCursor::pos()));
+    if (qobject_cast<QPushButton *>(child) )
+    {
+        setCursor(Qt::ArrowCursor);
+        // return;
+    }
+    if (resizing)
+    {
+        QPoint delta = event->globalPos() - mouseStartPos;
+        QRect newRect = windowStartRect;
+        if (resizeDirection & Qt::LeftEdge)
+            newRect.setLeft(windowStartRect.left() + delta.x());
+        if (resizeDirection & Qt::RightEdge)
+            newRect.setRight(windowStartRect.right() + delta.x());
+        if (resizeDirection & Qt::TopEdge)
+            newRect.setTop(windowStartRect.top() + delta.y());
+        if (resizeDirection & Qt::BottomEdge)
+            newRect.setBottom(windowStartRect.bottom() + delta.y());
+        // Enforce minimum size
+        if (newRect.width() >= minimumWidth() && newRect.height() >= minimumHeight())
+            setGeometry(newRect);
+    }
+    else if (mousePressed)
+    {
+        QPoint delta = event->globalPos() - mouseStartPos;
+        move(windowStartRect.topLeft() + delta);
+    }
+}
+
+void Widget::mouseReleaseEvent(QMouseEvent *event)
+{
+    // qDebug() << __PRETTY_FUNCTION__;
+    updateCursorShape(event->pos());
+    // Stop dragging when mouse is released
+    if (event->button() == Qt::LeftButton)
+    {
+        mousePressed = false;
+        resizing = false;
+    }
+}
+
+void Widget::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event);
+    QPainter painter(this);
+    QPen borderPen(Qt::darkGray);
+    borderPen.setWidth(1); // Thin border
+    painter.setPen(borderPen);
+    painter.drawRect(rect().adjusted(0, 0, -1, -1));
+}
+
 void Widget::moveEvent(QMoveEvent *event)
 {
+    // qDebug() << __PRETTY_FUNCTION__;
     // Chiamiamo la funzione di base per garantire che l'evento venga gestito normalmente
     QWidget::moveEvent(event);
     // 2. Chiama la funzione di salvataggio
@@ -175,6 +320,37 @@ void Widget::on_muteButton_clicked()
     }
 }
 
+void Widget::toggleOnTop()
+{
+    Qt::WindowFlags flags = windowFlags();
+    if (flags & Qt::WindowStaysOnTopHint)
+    {
+        flags &= ~Qt::WindowStaysOnTopHint;
+        onTopButton->setText("↑");
+    }
+    else
+    {
+        flags |= Qt::WindowStaysOnTopHint;
+        onTopButton->setText("↓");
+    }
+    setWindowFlags(flags);
+    show(); // Reapply flags
+}
+
+void Widget::toggleTransparency()
+{
+    if (windowOpacity() < 1.0)
+    {
+        setWindowOpacity(1.0);
+        transparentButton->setText("T");
+    }
+    else
+    {
+        setWindowOpacity(0.5); // Adjust as needed
+        transparentButton->setText("t");
+    }
+}
+
 void Widget::saveSettings()
 {
     // QSettings richiede il nome dell'Organizzazione e dell'Applicazione
@@ -184,6 +360,12 @@ void Widget::saveSettings()
     settings.setValue("Position", pos());
     // Potresti anche salvare la dimensione (larghezza, altezza)
     settings.setValue("Size", size());
+    Qt::WindowFlags flags = windowFlags();
+    if (flags & Qt::WindowStaysOnTopHint) settings.setValue("OnTop", true);
+    else settings.setValue("OnTop", false);
+    // if (windowOpacity() == 1.0) settings.setValue("Transparent", false);
+    // else settings.setValue("Transparent", true);
+    settings.setValue("Transparent", windowOpacity());
     // In Qt, le impostazioni vengono salvate automaticamente
 }
 
@@ -195,7 +377,7 @@ void Widget::loadSettings()
     // 2. Carica la dimensione
     QSize savedSize = settings.value("Size", QSize(100, 200)).toSize();
     // 3. Applica le impostazioni
-    this->setFixedWidth (ui->muteButton->width () + 24);
+    //this->setFixedWidth (ui->muteButton->width () + 24);
     resize(savedSize);
     // Controlla se la posizione salvata è visibile su qualsiasi schermo
     // (Utile se l'utente ha staccato un monitor).
@@ -210,4 +392,44 @@ void Widget::loadSettings()
         // La posizione di default (100, 100) verrà usata se la chiave non esiste
         move(QPoint(100, 100));
     }
+    bool onTop = settings.value("OnTop", false).toBool();
+    qreal opacity = settings.value("Transparent", 1.0).toReal();
+    Qt::WindowFlags flags = windowFlags();
+    if (onTop)
+    {
+        flags |= Qt::WindowStaysOnTopHint;
+        onTopButton->setText ("↑");
+    }
+    else
+    {
+        flags &= ~Qt::WindowStaysOnTopHint;
+        onTopButton->setText ("↓");
+    }
+    setWindowFlags(flags);
+    if (opacity < 1.0) transparentButton->setText("T");
+    else transparentButton->setText("t");
+    setWindowOpacity(opacity);
+}
+
+void Widget::updateCursorShape(const QPoint &pos)
+{
+    QRect rect = this->rect();
+    if (pos.x() <= resizeMargin && pos.y() <= resizeMargin)
+        setCursor(Qt::SizeFDiagCursor);
+    else if (pos.x() >= rect.width() - resizeMargin && pos.y() <= resizeMargin)
+        setCursor(Qt::SizeBDiagCursor);
+    else if (pos.x() <= resizeMargin && pos.y() >= rect.height() - resizeMargin)
+        setCursor(Qt::SizeBDiagCursor);
+    else if (pos.x() >= rect.width() - resizeMargin && pos.y() >= rect.height() - resizeMargin)
+        setCursor(Qt::SizeFDiagCursor);
+    else if (pos.x() <= resizeMargin)
+        setCursor(Qt::SizeHorCursor);
+    else if (pos.x() >= rect.width() - resizeMargin)
+        setCursor(Qt::SizeHorCursor);
+    else if (pos.y() <= resizeMargin)
+        setCursor(Qt::SizeVerCursor);
+    else if (pos.y() >= rect.height() - resizeMargin)
+        setCursor(Qt::SizeVerCursor);
+    else
+        setCursor(Qt::ArrowCursor);
 }
--- widget.h
+++ widget.h
@@ -1,11 +1,14 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
+#include "systemvolumecontroller.h"
 #include <QWidget>
 #include <QWidget>
 #include <QMouseEvent>
 #include <QCloseEvent>
-#include "systemvolumecontroller.h"
+#include <QPushButton>
+#include <QPainter>
+#include <QPen>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -22,12 +25,30 @@ public:
 private:
     Ui::Widget *ui;
     SystemVolumeController *m_systemVolumeController;
-    void updateMuteButtonIcon();
+    bool mousePressed;
+    bool resizing;
+    int resizeMargin;
+        int topButtonsMargin;
+void updateMuteButtonIcon();
     void saveSettings();
     void loadSettings();
+    QPoint mouseStartPos;
+    QPoint windowStartPos;
+    QPushButton *closeButton;
+    QPushButton *minimizeButton;
+    QPushButton *onTopButton;
+    QPushButton *transparentButton;
+    QRect windowStartRect;
+    Qt::Edges resizeDirection;
+    void updateCursorShape(const QPoint &pos);
 protected:
     void moveEvent(QMoveEvent *event) override;
     void closeEvent(QCloseEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
 private slots:
     void onSystemVolumeChanged(float newVolume);
     void onSystemMuteChanged(bool muted);
@@ -35,5 +56,7 @@ private slots:
     void onDeviceChanged(const QString &deviceId, const QString &friendlyName);
     void on_volumeSlider_valueChanged(int value);
     void on_muteButton_clicked();
-};
+    void toggleOnTop();
+ void toggleTransparency();
+ };
 #endif // WIDGET_H
